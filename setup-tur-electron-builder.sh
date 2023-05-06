1aa9081ac699fe40e671eaa72af2f47a9b09ceed
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-05-06 15:30:54 +0800

    chore: refactor

---
+++ setup-tur-electron-builder.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# setup-tur-electron-builder.sh - script to setup an electron building environment
+
+set -e -u -o pipefail
+
+# Enter the working directory
+basedir=$(realpath $(dirname $0))
+cd $basedir
+
+# Checkout the master branch of termux/termux-packages
+if [ -d "./termux-packages" ] && [ -d "./termux-packages/.git" ]; then
+	echo "Pulling termux-packages..."
+	pushd ./termux-packages
+	git reset --hard origin/master
+	git pull --rebase
+	popd
+else
+	rm -rf ./termux-packages
+	git clone https://github.com/termux/termux-packages.git --depth=1
+fi
+
+# Checkout the master branch of termux-user-repository/tur
+if [ -d "./tur-repo" ] && [ -d "./tur-repo/.git" ]; then
+	echo "Pulling termux-packages..."
+	pushd ./termux-packages
+	git reset --hard origin/master
+	git pull --rebase
+	popd
+else
+	rm -rf ./tur-repo
+	git clone https://github.com/termux-user-repository/tur.git tur-repo --depth=1
+fi
+
+# Push changes
+git stash -u
+
+# Delete other files
+shopt -s extglob
+rm -rf !(termux-packages|tur-repo)
+shopt -u extglob
+
+# Reset to current branch
+git reset --hard
+
+# Merge files
+cp -rn ./termux-packages/* ./
+cp -rn ./tur-repo/* ./
+
+# Pop changes
+git stash pop
+
+# Apply script patches
+shopt -s nullglob
+_patch=
+for _patch in ./common-files/building-system-patches/*.patch; do
+	echo "Applying patch: $_patch"
+	patch --silent -p1 < $_patch
+done
+unset _patch
+shopt -u nullglob
+
+# Remove files
+rm -f build-package.sh.orig
